d746a198c0418aff51ed6c19422e1be4e146caff
Student <example@example.com>
2019-09-25 16:26:08 -0700

    Add UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import updateUser from './actions/user-actions';
 
 var testStyle = {
   textAlign: 'center'
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  onUpdateUser() {
+    this.props.onUpdateUser('Sam');
+    console.log('clicked');
+  }
+
   render() {
     console.log(this.props);
+
     return (
       <div>
-      <h1 style={testStyle}>HELLO WORLD</h1>
+      <h1 style={testStyle}>{this.props.user}</h1>
+      <input onChange={this.onUpdateUser}/>
+      <div onClick={this.onUpdateUser}>Update</div>
+      {this.props.user}
       </div>
     );
   }
@@ -23,7 +39,7 @@ user: state.user
 });
 
 const mapActionsToProps = {
-
+onUpdateUser: updateUser
 }
 
 export default connect(mapStateToProps, mapActionsToProps)(App);
